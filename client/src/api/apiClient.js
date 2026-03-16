9602de30b3ea33ec9d35782be4addf45562f3743
Prabhat Kumar <prabhat.kumar.hisar@gmail.com>
2026-03-16 18:58:31 +0530

    update:apiClient

--- client/src/api/apiClient.js
+++ client/src/api/apiClient.js
@@ -2,14 +2,12 @@ import axios from "axios";
 import { getToken } from "../utils/authStorage";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 apiClient.interceptors.request.use(
   (config) => {
     const token = getToken();
-
-    console.log("Token being sent:", token);
 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
